fa98dcb2951fc17733a5efef23c2ef5cef839efd
thatcherlane <thatcherlane@gmail.com>
2018-02-15 22:20:33 -0900

    passes case #4

--- stringCalcKata/stringCalcKata/StringCalculatorTests.cpp
+++ stringCalcKata/stringCalcKata/StringCalculatorTests.cpp
@@ -20,6 +20,7 @@ int ParseString(std::string inputStr)
     if(inputStr.empty()){
         return 0;
     }
+    
     else if(inputStr.find(',') != std::string::npos) {
         std::stringstream ss(inputStr);
 
@@ -42,12 +43,38 @@ int ParseString(std::string inputStr)
         return sum;
 
     }
+
+    else if (inputStr.find('\n') != std::string::npos){
+        std::stringstream ss(inputStr);
+
+        std::vector<int> individualNumbers;
+
+        while(ss.good())
+        {
+            std::string temp;
+            getline(ss,temp,'\n');
+            int tempInt = std::stoi(temp);
+            individualNumbers.push_back(tempInt);
+        }
+
+        int sum = 0;
+        for(const auto & n : individualNumbers)
+        {
+            sum += n;
+        }
+
+        return sum;
+    }
+
     else{
         int retVal = std::stoi(inputStr);
         return retVal;
     }
+
     return -1;
 }
+
+
 
 TEST_CASE("Empty string returns 0", "[StringEval]") {
     REQUIRE(ParseString("") == 0);
@@ -61,4 +88,9 @@ TEST_CASE("Single string returns its value", "[StringEval]") {
 TEST_CASE("Two numbers comma delimited, returns the sum", "[StringEval]"){
     REQUIRE(ParseString("1,2") == 3);
     REQUIRE(ParseString("1,3") == 4);
+}
+
+TEST_CASE("Two numbers delimited by a new line, returns the sum", "[StringEval]"){
+    REQUIRE(ParseString("1\n2") == 3);
+    REQUIRE(ParseString("1\n3") == 4);
 }
