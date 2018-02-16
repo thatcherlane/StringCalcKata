4111cf6c48f393cad63b8edd03f1fef918a40cb3
thatcherlane <thatcherlane@gmail.com>
2018-02-15 21:52:44 -0900

    Passes second test

--- stringCalcKata/stringCalcKata/StringCalculatorTests.cpp
+++ stringCalcKata/stringCalcKata/StringCalculatorTests.cpp
@@ -11,17 +11,27 @@
 
 #include "StringCalculator.h"
 
+#include <string>
 #include <sstream>
 
 
 int ParseString(std::string inputStr)
 {
     if(inputStr == ""){
-        return 0; 
+        return 0;
+    }
+    else{
+        int retVal = std::stoi(inputStr);
+        return retVal;
     }
     return -1;
 }
 
 TEST_CASE("Empty string returns 0", "[StringEval]") {
     REQUIRE(ParseString("") == 0);
+}
+
+TEST_CASE("Single string returns its value", "[StringEval]") {
+    REQUIRE(ParseString("1") == 1);
+    REQUIRE(ParseString("2") == 2); 
 }
